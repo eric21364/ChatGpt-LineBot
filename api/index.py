abeeec7eb242c3e15d8c868740e337c0bc54c88a
eric21364 <eric21364@gmail.com>
2023-02-27 21:53:15 +0800

    調整判斷式

--- api/index.py
+++ api/index.py
@@ -8,7 +8,8 @@ import os
 
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
-working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+working_status = {}
+os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
 
 app = Flask(__name__)
 chatgpt = ChatGPT()
@@ -39,22 +40,23 @@ def handle_message(event):
     
     if event.message.type != "text":
         return
-    
+    if not event.reply_token in working_status:
+        working_status[event.reply_token] = True
     if event.message.text == "啟動":
-        working_status = True
+        working_status[event.reply_token] = True
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="我是時下流行的AI智能，目前可以為您服務囉，歡迎來跟我互動~"))
         return
 
     if event.message.text == "安靜":
-        working_status = False
+        working_status[event.reply_token] = False
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage(text="感謝您的使用，若需要我的服務，請跟我說 「啟動」 謝謝~"))
         return
     
-    if working_status:
+    if working_status[event.reply_token]:
         chatgpt.add_msg(f"Human:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
